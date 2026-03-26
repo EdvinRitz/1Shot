f9912d749342e65a9da56192c56d12cb9b81f909
EdvinRitz <edvin.ritzler@gmail.com>
2026-03-26 21:46:59 +0100

    Add Shoot action and WeaponShoot behavior
    Introduce a new Shoot input action and hook up weapon firing logic. Updated the InputSystem.inputactions and regenerated InputSystem.cs to add the Player/Shoot action and bindings (mouse left button and controller right shoulder) and removed legacy Attack bindings. PlayerInputHandler now exposes a shootPressed flag and subscribes to Shoot.performed. Added a WeaponShoot MonoBehaviour (and attached it to the Weapon GameObject in SampleScene) that reads the flag, performs a raycast from the first-person camera, logs hits and resets the flag. Also updated the scene asset to wire the new component and references.

--- Assets/Scripts/Weapon/WeaponShoot.cs
+++ Assets/Scripts/Weapon/WeaponShoot.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour
 {
+    public PlayerInputHandler playerInputHandler;
+    public Camera fpCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,18 @@ public class WeaponShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerInputHandler.shootPressed == true)
+        {
+            Debug.Log("shot");
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit);
+        Debug.Log(hit.transform);
+        playerInputHandler.shootPressed = false;
     }
 }
