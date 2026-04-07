3ef483b327ceb4f8120b6796e775e507698f25cc
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-07 09:43:41 +0200

    Shotgunner rotates towards player
    Shoutgunner now rotates towards the Player, small test

--- Assets/Scripts/Enemy/Grunt/GruntEnemy.cs
+++ Assets/Scripts/Enemy/Grunt/GruntEnemy.cs
@@ -7,7 +7,6 @@ public class GruntEnemy : MonoBehaviour
     public UnityEngine.AI.NavMeshAgent Agent { get => agent; }
     private GameObject player;
     public GameObject Player { get => player; }
-    public GruntEnemy gruntEnemy;
     private StateMachine stateMachine;
     public float attackDistance = 6f;
     public float sightDistance = 20f;
@@ -25,7 +24,7 @@ public class GruntEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //agent.SetDestination(player.transform.position);
+        
     }
 
     public bool CanSeePlayer()
@@ -60,15 +59,4 @@ public class GruntEnemy : MonoBehaviour
     {
         stateMachine.ChangeState(new GruntDieState(this));
     }
-    //{
-        //agent.isStopped = true;
-        //StartCoroutine(DisableAfterDelay());
-    //}
-
-    //IEnumerator DisableAfterDelay()
-    //{
-        //yield return new WaitForSeconds(1f);
-        //gameObject.SetActive(false);
-    //}
-
 }
--- Assets/Scripts/Enemy/Shotgunner/ShotgunnerEnemy.cs
+++ Assets/Scripts/Enemy/Shotgunner/ShotgunnerEnemy.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class ShotgunnerEnemy : MonoBehaviour
 {
+    private ShotgunnerEnemy shotgunnerEnemy;
+    private UnityEngine.AI.NavMeshAgent agent;
+    public UnityEngine.AI.NavMeshAgent Agent { get => agent; }
+    private GameObject player;
+    public GameObject Player { get => player; }
+    private StateMachine stateMachine;
+    public Vector3 lookDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        stateMachine = GetComponent<StateMachine>();
+        stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
+        shotgunnerEnemy = GetComponent<ShotgunnerEnemy>();
+        //stateMachine.ChangeState(new GruntMoveState(this));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lookDirection = (shotgunnerEnemy.Player.transform.position - shotgunnerEnemy.transform.position).normalized;
+        shotgunnerEnemy.transform.rotation = Quaternion.LookRotation(lookDirection);
     }
 }
