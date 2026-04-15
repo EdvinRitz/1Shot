a8442a464de9f01a40d3bc7931031d9d08a1aea0
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-15 22:02:37 +0200

    Bullet multiple hits bug
    Fixed a bug with the bullet where multiple hits was registered due to the size of the bulled. Made a safeguard to garantee that only one hit is registered

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    bool hasHit = false;
     private void OnCollisionEnter(Collision collision)
     {
+        if (hasHit)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Transform hittTransform = collision.transform;
         if (hittTransform.CompareTag("Player"))
         {
+            hasHit = true;
             Debug.Log("Hit Player");
             //hittTransform.GetComponent<PlayerHealth>().TakeDamage(10);
             Destroy(gameObject);
