c495bb93afc5080c697dbd915ccce5b966c0aaa3
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-17 20:33:00 +0200

    Empty ShotgunnerPanicState
    Created an empty shotgunner panic state with a constructor

---
+++ Assets/Scripts/Enemy/States/ShotgunnerStates/ShotgunnerPanicState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ShotgunnerPanicState : BaseState
+{
+    readonly ShotgunnerEnemy shotgunnerEnemy;
+    public ShotgunnerPanicState(ShotgunnerEnemy shotgunnerEnemy)
+    {
+        this.shotgunnerEnemy = shotgunnerEnemy;
+    }
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Perform()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
