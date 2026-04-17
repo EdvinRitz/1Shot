a1a1c4075af2ecbb690b5bac77a7bd145a72a4c3
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-17 14:13:18 +0200

    Empty Shielder move state
    Created a empty shielder move state with constructor

---
+++ Assets/Scripts/Enemy/States/ShielderStates/ShielderMoveState.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ShielderMoveState : BaseState
+{
+    ShielderEnemy shielderEnemy;
+
+    public ShielderMoveState(ShielderEnemy shielderEnemy)
+    {
+        this.shielderEnemy = shielderEnemy;
+    }
+
+    public override void Enter()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Perform()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
