e4840f214e3f92fb66d9a28fb5076a10e9dcf42e
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-19 20:10:30 +0200

    Update panic path continiusly
    Updates the panic path every frame, adjusting to the players positions and inputs, instead of once at Enter(). Also gave player a Nav Mesh Obstacle component

--- Assets/Scripts/Enemy/States/ShotgunnerStates/ShotgunnerPanicState.cs
+++ Assets/Scripts/Enemy/States/ShotgunnerStates/ShotgunnerPanicState.cs
@@ -5,6 +5,7 @@ public class ShotgunnerPanicState : BaseState
     readonly ShotgunnerEnemy shotgunnerEnemy;
     Vector3 panicDirection;
     float normalSpeed;
+    Vector3 initialPostition;
     public ShotgunnerPanicState(ShotgunnerEnemy shotgunnerEnemy)
     {
         this.shotgunnerEnemy = shotgunnerEnemy;
@@ -14,13 +15,15 @@ public class ShotgunnerPanicState : BaseState
         normalSpeed = shotgunnerEnemy.Agent.speed;
         shotgunnerEnemy.Agent.speed = shotgunnerEnemy.Agent.speed * 2;
         panicDirection = (shotgunnerEnemy.Player.transform.position - shotgunnerEnemy.transform.position).normalized;
-        shotgunnerEnemy.Agent.SetDestination(shotgunnerEnemy.transform.position + panicDirection * 10f);
+        initialPostition = shotgunnerEnemy.transform.position;
+        //shotgunnerEnemy.Agent.SetDestination(shotgunnerEnemy.transform.position + panicDirection * 10f);
     }
 
     public override void Perform()
     {
         //panicDirection = -(shotgunnerEnemy.Player.transform.position - shotgunnerEnemy.transform.position).normalized;
         //shotgunnerEnemy.Agent.SetDestination(shotgunnerEnemy.transform.position + panicDirection * 3f);
+        shotgunnerEnemy.Agent.SetDestination(initialPostition + panicDirection * 10f);
     }
 
     public override void Exit()
