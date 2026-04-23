cb144c8686fe3884fe666d1c502ba431ca6ffc8f
EdvinRitz <edvin.ritzler@gmail.com>
2026-04-23 19:11:00 +0200

    damage logic
    Put code in the PlayerHealth script that handles taking damage and healing logic

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    private float health;
+    public float maxHealth = 5;
+    private float lerpTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+        public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+        Debug.Log(health);
+    }
+
+        public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+        Debug.Log(health);
     }
 }
