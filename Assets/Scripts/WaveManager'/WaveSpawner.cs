90784810b98dd46fc586a07b4f676ecd1ea41c91
EdvinRitz <edvin.ritzler@gmail.com>
2026-06-10 19:23:17 +0200

    round robin
    Now spawns enemies in a wave in a round robin order

--- Assets/Scripts/WaveManager'/WaveSpawner.cs
+++ Assets/Scripts/WaveManager'/WaveSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
@@ -9,19 +10,24 @@ public class Wave
 public class WaveSpawner : MonoBehaviour
 {
     public Wave[] waves;
+    public Transform[] spawnPoints;
     public GameObject enemyMovingPrefab;
     public Transform spawnPoint1;
     private GameObject spawnedEnemy;
+    private int currentWaveIndex;
+    private bool waveActive;
+    private List<GameObject> spawnedEnemies = new();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentWaveIndex = 0;
         //spawnedEnemy = Instantiate(enemyMovingPrefab, spawnPoint1.position, spawnPoint1.rotation);
-        foreach (GameObject enemy in waves[0].enemiesToSpawn)
-        {
-            Instantiate(enemy, spawnPoint1.position, spawnPoint1.rotation);
-        }
+        //foreach (GameObject enemy in waves[0].enemiesToSpawn)
+        //{
+           // Instantiate(enemy, spawnPoint1.position, spawnPoint1.rotation);
+        //}
 
-
+        StartWave();
         //foreach (RaycastHit hit in orderedHitsByDistance)
     }
 
@@ -36,6 +42,13 @@ public class WaveSpawner : MonoBehaviour
 
     private void StartWave()
     {
-        
+        int enemyIndex = 0;
+        spawnedEnemies = new();
+        foreach (GameObject enemy in waves[currentWaveIndex].enemiesToSpawn)
+        {
+            Transform spawnPoint = spawnPoints[enemyIndex % spawnPoints.Length];
+            Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+            enemyIndex++;
+        }
     }
 }
